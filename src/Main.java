95bbf8269a273bb3b87161e72599e5ff47483f31
olwinter <olgarazgulyaeva@yandex.ru>
2022-02-04 19:22:29 +0300

    Add project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 456;
+        int priceMile = 20;
+        int mile;
+
+        mile = ticket / priceMile;
+
+        System.out.println((mile) + " - количество милей за билет");
+    }
+}
+
